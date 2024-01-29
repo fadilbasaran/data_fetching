28aca2e0c1c871b76c6173e6f45cdff1a415a250
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-29 18:55:43 +0300

    Fetch and cache stratejileri

--- data-fetching/app/page.js
+++ data-fetching/app/page.js
@@ -2,13 +2,30 @@
 import React from 'react'
 
 export const getData = async ()=>{
-  const res= await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const res= await fetch('https://jsonplaceholder.typicode.com/posts/1',{
+  });
+  return res.json();
+
+}
+export const getOtherData = async ()=>{
+  const res= await fetch('https://jsonplaceholder.typicode.com/posts/2',{
+  });
+  return res.json();
+
+}
+export const getOtherData2 = async ()=>{
+  const res= await fetch('https://jsonplaceholder.typicode.com/posts/3',{
+  });
   return res.json();
 
 }
 async function HomePage() {
-  const data = await getData();
-  console.log(data.title)
+  const data =  getData();
+  const data2 =  getOtherData();
+  // const data3 =  getOtherData2();
+  // const [dataResults1,dataResults2,dataResults3]= await  Promise.all([ data,data2,data3]);
+
+  // console.log({dataResults1,dataResults2,dataResults3})
   return (
     <h1>Data Fetching</h1> 
   )
